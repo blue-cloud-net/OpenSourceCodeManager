be9b17d984ca8618f6461cfec989af44be8f150b
huansky <bluec@DESKTOP-2TR5A5R>
2021-10-17 23:23:10 +0800

    api和webpage服务创建

---
+++ OSCManager.Api/Controllers/SourceRepositoryController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace OSCManager.Api.Controllers
+{
+    [WebApi]
+    public class SourceRepositoryController : Controller
+    {
+    }
+}
